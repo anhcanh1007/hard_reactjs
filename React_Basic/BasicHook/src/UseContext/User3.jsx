9a68ddc16448300dcd554369f5955446669e21db
anhcanh1007 <anhcanh1007@gmail.com>
2025-03-15 23:48:47 +0700

    learn useContext

---
+++ React_Basic/BasicHook/src/UseContext/User3.jsx
@@ -0,0 +1,101 @@
+import React, { createContext, useEffect, useState } from "react";
+import UserProfile from "./UserProfile";
+
+const initialAddress = () => {
+  // console.log("initialAddress");
+  return {
+    nation: "viet nam",
+    city: {
+      street: "duong 36 linh dong",
+      house: "Building",
+    },
+  };
+};
+const getAddress = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        nation: "viet name",
+        city: {
+          street: "67 mai chi tho",
+          house: "Building",
+        },
+      });
+    }, 3000);
+  });
+};
+
+export const UserContext = createContext();
+
+export default function User3() {
+  const [name, setname] = useState("anh canh");
+  const [age, setAge] = useState(24);
+  const [address, setAddress] = useState(initialAddress);
+  const [, forceRerender] = useState(0);
+
+  const increAge = () => {
+    setAge((prevAge) => prevAge + 1);
+  };
+
+  const changeStreet = () => {
+    setAddress((prevAddress) => {
+      const newCity = { ...prevAddress.city };
+      newCity.street = "67 mai chi tho";
+      return {
+        ...prevAddress,
+        city: newCity,
+      };
+    });
+  };
+
+  const rerender = () => {
+    forceRerender((prevState) => prevState + 1);
+  };
+  //   console.log("rerender");
+
+  //trường hợp gọi useEffect không truyền deps, nó đóng vai trò như componentDidUpdate trong class component, nó được gọi sau khi component re-render
+  // useEffect(() => {
+  //   console.log("compnent did update");
+  // });
+
+  //trường hợp dùng useEffect có deps nhưng là aray rỗng, thì nó đóng vai trò như một componentDidMount, chỉ chạy một lần sau khi component mounted. vì vậy nó thường được dùng để gọi api
+  useEffect(() => {
+    getAddress().then((res) => {
+      setAddress((prevAddress) => {
+        const newAddress = { ...prevAddress };
+        newAddress.city = res.city;
+        return newAddress;
+      });
+    });
+
+    //clean up function
+    return () => {
+      // console.log("huy goi api");
+    };
+  }, []);
+
+  //trường hợp dùng useEffect có truyền deps và tham số trong deps thì mỗi lần tham số truyền vào trong deps thay đổi thì nó sẽ chạy useEffect
+  // useEffect(() => {
+  //   console.log(`age or name has change: count = ${age}, name: ${name}`);
+  //   return () => {
+  //     console.log("no flow");
+  //   };
+  // }, [age, name]);
+
+  return (
+    <div>
+      <UserContext.Provider
+        value={{
+          name,
+          age,
+          address,
+          increAge,
+        }}
+      >
+        <UserProfile />
+      </UserContext.Provider>
+      <button onClick={changeStreet}>Change Address</button>
+      <button onClick={rerender}>Rerender</button>
+    </div>
+  );
+}
