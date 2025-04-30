7fbd621ba6aeafec4ee840bfed37c651cd23c300
anhcanh1007 <anhcanh1007@gmail.com>
2025-04-30 22:58:26 +0700

    hoc useDebugValue

---
+++ ReactNangCao/UseDebugValue/src/Welcome/Welcome.tsx
@@ -0,0 +1,116 @@
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useDebugValue,
+  useMemo,
+  useState,
+} from "react";
+
+interface ThemeType {
+  theme: {
+    color: "red" | "blue";
+  };
+  onChangeColor: (color: "red" | "blue") => void;
+}
+
+const ThemeContext = createContext<ThemeType>({
+  theme: {
+    color: "red",
+  },
+  onChangeColor: () => {},
+});
+
+// custom hooks
+const useTheme = () => {
+  const [theme, setTheme] = useState<ThemeType["theme"]>({ color: "red" });
+  const onChangeColor = useCallback((color: "red" | "blue") => {
+    setTheme((prev) => ({ ...prev, color }));
+  }, []);
+  const doTask = (value: any) => {
+    for (let index = 0; index < 99999; index++) {}
+    return value === "red" ? "Theme is Red" : "Theme is blue";
+  };
+  useDebugValue(theme.color, doTask);
+  return { theme, onChangeColor };
+};
+//end custom hook
+
+export default function Welcome() {
+  const [, forceRender] = useState({});
+  const { theme, onChangeColor } = useTheme();
+
+  const valueContext = useMemo(
+    () => ({
+      theme,
+      onChangeColor,
+    }),
+    [theme, onChangeColor]
+  );
+  const force = () => {
+    forceRender({});
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={force}>Please Render Welcome</button>
+      </div>
+      <div style={{ backgroundColor: "gray", padding: "20px" }}>
+        <ThemeContext.Provider value={valueContext}>
+          <Form />
+          <Label />
+        </ThemeContext.Provider>
+      </div>
+    </div>
+  );
+}
+
+const Form = () => {
+  return (
+    <Panel title="Welcome">
+      <Button>Sign in</Button>
+      <Button>Log in</Button>
+    </Panel>
+  );
+};
+
+interface PanelProps {
+  title: string;
+  children: React.ReactNode;
+}
+const Panel = ({ title, children }: PanelProps) => {
+  const { theme } = useContext(ThemeContext);
+  const bg = theme.color;
+  return (
+    <div style={{ backgroundColor: `${bg}` }}>
+      <h1>{title}</h1>
+      <div style={{ display: "flex" }}>{children}</div>
+    </div>
+  );
+};
+
+interface ButtonProps {
+  children: React.ReactNode;
+}
+const Button = ({ children }: ButtonProps) => {
+  return (
+    <div>
+      <button>{children}</button>
+    </div>
+  );
+};
+
+const Label = () => {
+  const { theme, onChangeColor } = useContext(ThemeContext);
+  console.log("re-render");
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={theme.color === "blue"}
+        onChange={(e) => onChangeColor(e.target.checked ? "blue" : "red")}
+      />
+      <span>Use dark mode</span>
+    </div>
+  );
+};
---
+++ ReactNangCao/UseDebugValue/src/documend.md
@@ -0,0 +1,2 @@
+UseDebugvalue dùng để hỗ trợ debug nhanh hơn và dễ nhìn hơn trong component
+`useDebugValue` được dùng trong custom hook để hiển thị value lên React Dev Tool
